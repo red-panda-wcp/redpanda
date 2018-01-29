d00fbfc75ae49936d3905b5e15d45edf7bc373a4
安部望 <anbuwang@abenozomunoMacBook-Air.local>
2018-01-29 20:04:21 +0900

    validate2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :lastname, :firstname, :postal_code, :address, :phone, presence: true
-  validates :kana_lastname, :kana_firstname,  presence: true,
+  validates :lastname, :firstname, :postal_code, :address, :phone, presence: true,on: :create
+  validates :kana_lastname, :kana_firstname,  presence: true,on: :create,
   			 format: {
                    with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
                    message: "は全角カタカナで入力して下さい。"
                  }
-  validates :encrypted_password, presence: true, length: { maximum: 6 }
+  validates :encrypted_password, presence: true, length: { maximum: 6 },on: :create
+
 
 end
